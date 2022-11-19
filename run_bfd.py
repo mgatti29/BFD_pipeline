d062f049cfa15e80c0b3a6326a83394aac60dbf1
mgatti29 <mgatti@ifae.es>
2022-11-18 17:47:03 -0800

    labeling

--- run_bfd.py
+++ run_bfd.py
@@ -10,13 +10,18 @@ parser.add_argument('--config', type=str,
                     help='required config file')
 
 parser.add_argument('--tiles', nargs='+', type=str, help='required config file')
+parser.add_argument('--output_label', type=str, help='required config file')
+
+
 args = parser.parse_args()
 conf=BFD_pipeline.read_config(args.config)
+
 
 if args.tiles is not None:
     conf['measure_moments_targets']['tiles'] = [args.tiles]
 
-
+if args.output_label is not None:
+    conf['measure_moments_targets']['output_label'] = args.output_label
 
 
 BFD_pipeline.BFD_pipeline(conf)
