2cb37f242deac9741fd48d49f167688a347950a6
Marco Gatti <mgatti29@sas.upenn.edu>
2023-03-07 13:31:26 -0500

    fix exp

--- BFD_pipeline/measure_moments_targets.py
+++ BFD_pipeline/measure_moments_targets.py
@@ -625,9 +625,9 @@ def f(iii, config, params_template, chunk_size, path, tab_detections, m_array, b
 
                             # let's initialise it as [12 x numb_bands] --
                             
-                            orig_row_ = np.zeros(56)
-                            orig_col_ = np.zeros(56)
-                            ccd_name_ = -np.ones(56)
+                            orig_row_ = np.zeros(90)
+                            orig_col_ = np.zeros(90)
+                            ccd_name_ = -np.ones(90)
                             
                             
                             u_1 = np.array([ll[0] for l in tab_detections.images[index].orig_rowcol for ll in l[1:]])
@@ -718,9 +718,9 @@ def f(iii, config, params_template, chunk_size, path, tab_detections, m_array, b
 
                                         # let's initialise it as [12 x numb_bands] --
 
-                                        orig_row_ = np.zeros(56)
-                                        orig_col_ = np.zeros(56)
-                                        ccd_name_ =  -np.ones(56)
+                                        orig_row_ = np.zeros(90)
+                                        orig_col_ = np.zeros(90)
+                                        ccd_name_ =  -np.ones(90)
 
 
 
