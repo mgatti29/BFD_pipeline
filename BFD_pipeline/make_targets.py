7e6d3edeeaa77a3496649b60ddb75919f680c854
Marco Gatti <mgatti29@sas.upenn.edu>
2023-06-04 19:41:39 -0400

    Update make_targets.py

--- BFD_pipeline/make_targets.py
+++ BFD_pipeline/make_targets.py
@@ -31,15 +31,18 @@ from .createTiers import createTiers
 
 
 
-'''
-This
-'''
+
 
 
 
 
 def make_targets(output_folder,**config):
-    
+    '''
+    This stage of the pipeline loads into memory all the files with the measured targets moments and assemble them into one big file. It further assigns noise_tiers to each of the target.
+    It works both on simulated and real data.
+    '''
+
+    # it begins checking if a number of keywords are provide din the config file. If not, it sets them up to the default values
     try:
         config['classes']
     except:
@@ -49,24 +52,30 @@ def make_targets(output_folder,**config):
     try:
         config['re_run_noisetiers']
     except:
+        print ('Re-running the noisetiers code')
         config['re_run_noisetiers'] = True
         
     try:
         config['noiseStep']
     except:
+        print ('noiseStep not specified. I will set it up to 0.2 (default)')
         config['noiseStep'] = 0.2
     try:
         config['psfStep']
     except:
+        print ('psfStep not specified. I will set it up to 0.1 (default)')
         config['psfStep'] = 0.1
     try:
         config['match_to_gold']
     except:
+        print ('match_to_gold not specified. I will set it up to False (default)')
         config['match_to_gold'] = False
+        
     doit=False
+    
+    # checks on MPI -----
     if config['MPI']:
         from mpi4py import MPI 
-        
         comm = MPI.COMM_WORLD
         run_count =0
         
@@ -77,14 +86,16 @@ def make_targets(output_folder,**config):
         
         
     if doit:
-        
-        
         # the code needs to read in all the fits files, assign noisetiers, and save the final target file.
         # It distinguishes between image sims files and normal files.
+
+        # let's copy a few parameters from the config file.
         params_template = {}
         params_template['n'] = config['n'] 
         params_template['sigma'] =  config['sigma']     
-        add_labels = ['','ISp_','ISm_']
+
+        # the code will loop over 3 types of files: data, sims +, sims -.
+        add_labels = ['','ISp_','ISm_'] # if the label starts with IS, it refers to image-simulation files.
         
         # make the list for simulted tiles, they need to be loaded in the same order for noise cancelling.
         files_simulated_p = glob.glob(output_folder+'/targets/{1}{0}*.fits'.format('targets','ISp_'))
@@ -93,143 +104,116 @@ def make_targets(output_folder,**config):
         #print (add_labels)
         for add in add_labels:
             nlost = 0
-            
-            for xx,tt in enumerate(['targets']):
-                #'''
-                
-                
-                if not os.path.exists(output_folder+'/{1}{0}_sample_g.fits'.format(tt,add)):
-                    # read all the target files 
-                    files = glob.glob(output_folder+'/targets/{1}{0}*.fits'.format(tt,add))
 
-                    if add =='ISp_':
+            for xx, tt in enumerate(['targets']): # this for loop is not very useful, but I'd like to keep it this way in case of future development of the code. 
+                # Check if the FITS file for this combination of 'add' and 'tt' already exists
+                if not os.path.exists(output_folder+'/{1}{0}_sample_g.fits'.format(tt, add)):
+
+                    # Read all the target files based on the 'tt' and 'add' labels
+                    files = glob.glob(output_folder+'/targets/{1}{0}*.fits'.format(tt, add))
+
+                    # If 'add' label is 'ISp_', use files_simulated_p; if 'add' label is 'ISm_', use files_simulated_m
+                    if add == 'ISp_':
                         files = files_simulated_p
-                    if add =='ISm_':
+                    if add == 'ISm_':
                         files = files_simulated_m
+
                     list_target = []
 
-
-
-                    if len(files)>0:
+                    if len(files) > 0:
                         run_count = 0
 
                         hdulist = fits.HDUList([fits.PrimaryHDU()])
                         mute = fits.open(files[0])
                         names = [mute[1].columns[i].name for i in range(len(mute[1].columns))]
-                        
-                        
+
                         results_ = dict()
 
+                        # Process the data from each file and store it in the 'results_' dictionary
                         for ii in frogress.bar(range(len(files))):
                             file = files[ii]
                             mute = fits.open(file)
+
                             for cname in mute[1].columns.names:
                                 if ii == 0:
                                     results_[cname] = mute[1].data[cname]
                                 else:
                                     try:
-                                        if mute[1].data[cname].shape[1]>1:
-                                            results_[cname] = np.vstack([results_[cname],mute[1].data[cname]])
+                                        if mute[1].data[cname].shape[1] > 1:
+                                            results_[cname] = np.vstack([results_[cname], mute[1].data[cname]])
                                         else:
-                                            results_[cname] = np.hstack([results_[cname],mute[1].data[cname]])
+                                            results_[cname] = np.hstack([results_[cname], mute[1].data[cname]])
                                     except:
-                                 
-                                        results_[cname] = np.hstack([results_[cname],mute[1].data[cname]])
-                        mask =( ~np.isnan(results_['covariance'][:,0]))& (results_['covariance'][:,0]>0.) & (results_['covariance'][:,0]!=np.inf)
+                                        results_[cname] = np.hstack([results_[cname], mute[1].data[cname]])
+
+                        # Apply a mask to filter the data based on certain conditions
+                        mask = (~np.isnan(results_['covariance'][:, 0])) & (results_['covariance'][:, 0] > 0.) & (results_['covariance'][:, 0] != np.inf)
+
                         if config['match_to_gold']:
-                            gold =  np.load('/global/cfs/cdirs/des/BFD_Y6/gold_id.npy',allow_pickle=True)
-                            match = np.in1d(results_['id'],gold)
+                            gold = np.load('/global/cfs/cdirs/des/BFD_Y6/gold_id.npy', allow_pickle=True)
+                            match = np.in1d(results_['id'], gold)
                             mask = mask & match
-                                    
-                    
-                        print (len(mask[mask]),len(mask))
-                        # match to gold if needed:
-                        
+
+                        print(len(mask[mask]), len(mask))
+
+                        # Create FITS columns for additional data, if required
                         cols = []
                         if not ('AREA' in names):
-                            cols.append(fits.Column(name="AREA",format="K",array=0*np.ones_like(mask[mask])))
-                        cols.append(fits.Column(name="NOISETIER",format="K",array=0*np.ones_like(mask[mask])))
-                        
+                            cols.append(fits.Column(name="AREA", format="K", array=0*np.ones_like(mask[mask])))
+                        cols.append(fits.Column(name="NOISETIER", format="K", array=0*np.ones_like(mask[mask])))
+
                         new_cols = fits.ColDefs(cols)
                         hdu = fits.BinTableHDU.from_columns(mute[1].columns+new_cols)
-             
-            
-                        for key in (hdrkeys['weightN'],hdrkeys['weightSigma']):
+
+                        # Copy necessary header keywords from the original file
+                        for key in (hdrkeys['weightN'], hdrkeys['weightSigma']):
                             hdu.header[key] = mute[0].header[key]
-                        
+
+                        # Copy processed data to the new FITS file
                         for cname in results_.keys():
                             hdu.data[cname] = results_[cname][mask]
 
-
-                                
-                        #mask= (~np.isnan(occ)) & (occ>0.) & (~np.isnan(mf)) & match#& (noisetier == NOISE_TIER_MASK)
-                        
-                        
-  
-                        
-                        #I think we can assign random values that are different from nan here ****
-                        #####indx_ = np.arange(hdu.data['covariance'].shape[0])[mask]
-                        #####indx = indx_[np.random.randint(0,len(indx_),len(mask[~mask]))]
-                        #####hdu.data['covariance'][~mask,:] = hdu.data['covariance'][indx,:]
-
-                        ######if 1 ==1:
-                        ######    mask__ = (~mask) 
-                        ######    hdu.data['moments'][mask__,:] = 0.
-                        ######    #hdu.data['covariance'][mask__,:] = 1. # I think we can assign random values that are different from nan here ****
-                        ######    hdu.data['AREA'][mask__] = -1.
-                        ######    
-                        
-    
-                        for key in (hdrkeys['weightN'],hdrkeys['weightSigma']):
+                        # Copy necessary header keywords from the original file to the primary header
+                        for key in (hdrkeys['weightN'], hdrkeys['weightSigma']):
                             hdulist[0].header[key] = mute[0].header[key]
+
                         hdulist[0].header['STAMPS'] = mute[0].header['STAMPS']
-                        #hdulist[0].header['NLOST'] = nlost
-                 
+
+                        # Append the new HDU to the HDU list
                         hdulist.append(hdu.copy())
                         del hdu
 
                         try:
-                            hdulist.writeto(output_folder+'/{1}{0}_sample_g.fits'.format(tt,add))
+                            # Write the HDU list to the output FITS file
+                            hdulist.writeto(output_folder+'/{1}{0}_sample_g.fits'.format(tt, add))
                         except:
                             try:
-                                hdulist.writeto(output_folder+'/{1}{0}_sample_g.fits'.format(tt,add),clobber = True)# 
+                                # If the file write fails, try writing with the 'clobber' parameter set to True
+                                hdulist.writeto(output_folder+'/{1}{0}_sample_g.fits'.format(tt, add), clobber=True)
                             except:
-                                pass
-    
+                                pass    
         # MAKE NOISE TIERS *******************************************************************************
         
-
-                    
-        
+        # this understands if I need to run the noisetier snippet on the simulated or real data file.
         filex = (output_folder+'/{1}{0}_sample_g.fits'.format(tt,'ISp_'))
         if os.path.exists(filex):
             pass
         else:
             filex = (output_folder+'/{1}{0}_sample_g.fits'.format(tt,''))
-            
-            
-        
-        
-        print ('')
-        print (filex)
-        print ('')
 
-        
+        # Create the noisetier file if 're_run_noisetiers' flag is set and the file already exists, remove it
         if config['re_run_noisetiers']:
             if os.path.exists(output_folder+'/noisetiers.fits'):
                 os.remove(output_folder+'/noisetiers.fits')
-
-            tc = createTiers([filex],config['noiseStep'],config['psfStep'],config['Mf_min'],config['Mf_max'],config['sn_min'],config['sn_max'])
+            tc = createTiers([filex], config['noiseStep'], config['psfStep'], config['Mf_min'], config['Mf_max'], config['sn_min'], config['sn_max'])
             tc.save(output_folder+'/noisetiers.fits')
-            
-            # os.system('python BFD_pipeline/createTiers.py {0} --snMin {1} --snMax  {2} --fluxMin {3} --fluxMax {4} --noiseStep {6} --psfStep {7} --output {5}'.format(filex,config['sn_min'],config['sn_max'],config['Mf_min'],config['Mf_max'],output_folder+'/noisetiers.fits',config['noiseStep'],config['psfStep']))
         else:
+            # Create the noisetier file if it doesn't already exist
             if not os.path.exists(output_folder+'/noisetiers.fits'):
-                # os.system('python BFD_pipeline/createTiers.py {0} --snMin {1} --snMax  {2} --fluxMin {3} --fluxMax {4}  --noiseStep {6} --psfStep {7}  --output {5}'.format(filex,config['sn_min'],config['sn_max'],config['Mf_min'],config['Mf_max'],output_folder+'/noisetiers.fits',config['noiseStep'],config['psfStep']))
-                
-                tc = createTiers([filex],config['noiseStep'],config['psfStep'],config['Mf_min'],config['Mf_max'],config['sn_min'],config['sn_max'])
+                tc = createTiers([filex], config['noiseStep'], config['psfStep'], config['Mf_min'], config['Mf_max'], config['sn_min'], config['sn_max'])
                 tc.save(output_folder+'/noisetiers.fits')
-        
+
+
         # ASSIGN NOISE TIERS ********************
         from bfd import TierCollection
 
@@ -260,15 +244,9 @@ def make_targets(output_folder,**config):
                     
                     mask = (produce_sn(m_)>config['sn_min']) & (produce_sn(m_)< config['sn_max']) & (produce_mf(m_)>config['Mf_min']) & (produce_mf(m_)< config['Mf_max']) 
 
-
-                    
-                    
-
                     # add class column for later **********************************
                     # we want to use the worst noise tiers for this!!
-                    
-                    
-                    
+
                     phi = -0.5*(angle(m_[1].data['moments'].T))
                     e_,o_ = rotate(m_[1].data['moments'].T,m_[1].data['moments'][:,:2].T,phi)
                     new_DV_targets = np.hstack([e_,o_])
